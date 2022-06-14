c2d5e9b5e0d008c69a93b25224758495bca53da8
Hunter <46440327+Hunter2809@users.noreply.github.com>
2022-06-14 23:03:32 +0530

    create TODO

--- utils/hunai.py
+++ utils/hunai.py
@@ -1,8 +1,22 @@
+from disnake import Intents
 from disnake.ext import commands
 from AI.ai import AI
 
 
 class HunAI(commands.Bot):
     def __init__(me):
-        super().__init__(["hun", "Hun"])
+        super().__init__(
+            command_prefix=["hun", "Hun"],
+            intents=Intents(
+                members=True,
+                message_content=True,
+                guilds=True,
+                messages=True,
+            ),
+        )
         me.ai_session = AI()
+
+    def on_ready(me):
+        print(
+            f"The bot has started up with {len(me.users)} users from {len(me.guilds)} guilds chatting with me!"
+        )
